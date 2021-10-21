5dd46f9954e2dc1ead7e5c7f8fa09ff9ecfb534f
IraMatsuk <g3726866@gmail.com>
2021-10-21 13:36:48 +0300

    feat: add own exception for triangle

---
+++ src/main/java/by/matsukiryna/shapetask/exception/ShapeException.java
@@ -0,0 +1,20 @@
+package by.matsukiryna.shapetask.exception;
+
+public class ShapeException extends Exception {
+    public ShapeException(){}
+
+    public ShapeException(String message) {
+        super(message);
+    }
+
+    public ShapeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShapeException(Throwable cause) {
+        super(cause);
+    }
+
+    public ShapeException(String s, double x, double y, double z, double height, double radius) {
+    }
+}
